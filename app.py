d357d42f40b4b0ed43c64b4cdc4533247af01f4e
peerapatkulwong <80526263+peerapatkulwong@users.noreply.github.com>
2022-05-18 00:20:26 +0700

    make it better

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template, request
-from turtle import *
 import turtle
 app = Flask(__name__, template_folder = 'template')
 
 @app.route("/",methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        length = str(request.form.get('pw'))
-        if length == 'triangle':
-            geo = turtle.Turtle()
-            geo.forward(100)
-            geo.left(120)
-            geo.forward(100)
-            geo.left(120)
-            geo.forward(100)
-            turtle.done()
-            return render_template("index.html")
-    else:
-        return render_template("index.html")
+        color = str(request.form.get('co'))
+        length = int(request.form.get('le'))
+        geo = turtle.Turtle()
+        geo.color(color)
+        geo.forward(length)
+        geo.left(120)
+        geo.forward(length)
+        geo.left(120)
+        geo.forward(length)
+        turtle.done()
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
