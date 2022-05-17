6bd9f59f2141066b044ebe41ef90da7a4541df1a
peerapatkulwong <80526263+peerapatkulwong@users.noreply.github.com>
2022-05-18 04:32:28 +0700

    first commit

--- app.py
+++ app.py
@@ -7,15 +7,20 @@ def index():
     if request.method == 'POST':
         color = str(request.form.get('co'))
         length = int(request.form.get('le'))
-        geo = turtle.Turtle()
-        geo.color(color)
-        geo.forward(length)
-        geo.left(120)
-        geo.forward(length)
-        geo.left(120)
-        geo.forward(length)
-        turtle.done()
-    return render_template("index.html")
+        if length >= 100:
+            geo = turtle.Turtle()
+            geo.color(color)
+            geo.forward(length)
+            geo.left(120)
+            geo.forward(length)
+            geo.left(120)
+            geo.forward(length)
+            turtle.done()
+            return render_template("index.html")
+        elif length <= 100:
+            return "<p>error</p>"
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
